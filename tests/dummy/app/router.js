0c79f4a3ecf608260ea9307d7aa598d6e9b0e8b1
abhilashlr <abhilashlr.789@gmail.com>
2020-04-07 20:55:21 +0530

    Adds ember-cli-addon-docs

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,15 @@
-import EmberRouter from '@ember/routing/router';
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
 import config from './config/environment';
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
+const Router = AddonDocsRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+});
 
 Router.map(function() {
+  docsRoute(this, function() { /* Your docs routes go here */ });
+
+  this.route('not-found', { path: '/*path' });
 });
+
+export default Router;
---
+++ tests/dummy/app/templates/docs/index.md
@@ -0,0 +1,9 @@
+# Basic implementation
+
+{{#docs-demo as |demo|}}
+  {{#demo.example name="docs-demo-basic.hbs"}}
+    <HOT::Grid />
+  {{/demo.example}}
+
+  {{demo.snippet "docs-demo-basic.hbs"}}
+{{/docs-demo}}
